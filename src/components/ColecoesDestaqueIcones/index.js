d3fde12f20770cbd9d8af123ab6847d9fa92e38c
n3vill <samthvet@hotmail.com>
2023-03-17 10:18:22 -0300

    deu ceeeertooo

--- src/components/ColecoesDestaqueIcones/index.js
+++ src/components/ColecoesDestaqueIcones/index.js
@@ -15,21 +15,32 @@ import React from "react";
 
 export default function ColecoesDestaqueIcones (){
 
+    const [ativo,setAtivo] = React.useState(0)
+
     const colecoes = [
-  { nome: "Camisetas", imagem: camisetas },
-  { nome: "Calças", imagem: calcas },
-  { nome: "Bonés", imagem: bones },
-  { nome: "Headphones", imagem: fones},
-  { nome: "Tênis", imagem: tenisi },
+  { nome: "Camisetas", imagem: camisetas, imagemAtiva: camisetasrosa},
+  { nome: "Calças", imagem: calcas, imagemAtiva: calcasrosa },
+  { nome: "Bonés", imagem: bones, imagemAtiva: bonesrosa },
+  { nome: "Headphones", imagem: fones, imagemAtiva: fonesrosa},
+  { nome: "Tênis", imagem: tenisi, imagemAtiva: tenisirosa },
 ];
 
+// const isActive = ( ) => {
+//     if (cada.nome === isActive) {
+//         return "active";
+//     }
+
+//     return "";
+// };
+
+
 const Sections = () => {
-    return colecoes.map((cada) => {
+    return colecoes.map((cada, posicao) => {
         return (
 
-            <Grid item className="iconeslinks">
+            <Grid item className="iconeslinks" onClick={() => setAtivo(posicao)}>
                 <Avatar sx={{ bgcolor: "white", height: "104px", width: "104px" }}>
-                        <img src={cada.imagem}></img>
+                        <img src={(posicao === ativo) ? cada.imagemAtiva : cada.imagem}></img>
                 </Avatar>
                 <Link to="">{cada.nome}</Link>
             </Grid>
@@ -37,13 +48,11 @@ const Sections = () => {
     }); 
 }
 
-    const [iproduto, setIproduto] = React.useState (0)
-
     return (
         <>
           <div className="bgGray">
              <Container>
-                    <h3>Coleções em destaque</h3>
+                    <h3 className="h3dest">Coleções em destaque</h3>
                     <Grid container  direction="row" justifyContent="center" alignItems="center"  spacing={2}> 
                         <Sections/>
                     </Grid>
