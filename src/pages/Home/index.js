aaacce6c7e3eb5cefaeaf785733fcb1c8a70e7ac
Carlianfernandes <carllyrocha@gmail.com>
2023-03-17 11:18:19 -0300

    atualização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,6 @@ export default function Home() {
             <Menu />
             <Carousel />
             <ColecoesDestaque />
-            <ColecoesDestaqueIcones/>
             <OfertaDestaque/>
             <Footer/>
         </>
