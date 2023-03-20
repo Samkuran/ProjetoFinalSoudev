91a18c1a8a4b11fbdf4152b1d629c47885bdd7ab
unknown <andysil.dgt23@gmail.com>
2023-03-20 00:07:52 -0300

    adicionado link faltante no componente

--- src/components/ProdutosEmAlta/index.js
+++ src/components/ProdutosEmAlta/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Container, Grid } from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
-import "./styles.css";
 import Produto from '../Produto';
+import "./styles.css";
 
 export default function ProdutosAlta () {
     return (
         <>
             <div className='bgGray'>
             <Container>
-                <h3>Produtos em alta</h3>
+                <div className='titulolinkpdt'>
+                    <h3>Produtos em alta</h3>
+                    <a href='' className='linkpdtalta'>Ver todos <ArrowRightAltIcon /></a>
+                </div>
+                
                 <Grid container>
                 <Produto />
                 <Produto />
