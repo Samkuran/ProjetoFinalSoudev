5b1eecefc9602db5f858e78abbaab1ee1553c668
Samantha Pimentel <samthvet@hotmail.com>
2023-03-19 23:22:51 -0300

    up nod produtos

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Card, CardContent, Container, Grid } from '@mui/material';
 
 import "./styles.css";
 import sapato1 from "./../ProdutosEmAlta/IMG/sapato1.png";
@@ -8,10 +8,16 @@ export default function Produto () {
     return (
         <>
             <Grid item xs={3} className='sapatoalta'>
-                <img src={sapato1} alt="" className='fotosapato' />
-                <span>Tenis Nike</span>
-                  <small className='precoantigo' xs={3}>R$200</small>
-                <small xs={3}>R$100</small>
+                
+                <Card sx={{ minWidth: 292, height: "321px", alignItems: "center", margin: 0, padding: 0}}>
+                        <img src={sapato1} alt="" className='fotosapato'/>
+                </Card>
+                <div className='textoSmall'>
+                    <span>Tênis</span>
+                    <h3>K-Swiss V8 - Masculino</h3>
+                    <small className='precoantigo' xs={3}>R$200</small>
+                    <small xs={3}><strong>R$100</strong></small>
+                </div>
             </Grid>
         </>
     )
